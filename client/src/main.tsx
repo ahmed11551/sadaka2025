50bd171dcc530062a7f68598cd7159c11c561e03
Ahmed DevOps <ahmed11551@users.noreply.github.com>
2025-12-07 07:34:46 +0300

    Initialize Telegram WebApp on app load

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initTelegramWebApp } from "./lib/telegram";
+
+// Инициализация Telegram WebApp при загрузке
+if (typeof window !== 'undefined') {
+  initTelegramWebApp();
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
